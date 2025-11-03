6a1c7b978844547de522bc22f857de2f335a9ece
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-11-03 10:37:17 +0100

    feat: all Class' functions are in the same file with the Class' name

--- cpp_module_01/ex04/replaceClass/Replace.cpp
+++ cpp_module_01/ex04/replaceClass/Replace.cpp
@@ -6,7 +6,7 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 13:27:27 by yukravch          #+#    #+#             */
-/*   Updated: 2025/11/03 10:22:31 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/11/03 10:36:04 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,58 @@ void	Replace::writeToNewFile(std::string replacedText) {
 	std::ofstream outfile(outfileName.c_str());
 	outfile << replacedText;
 	outfile.close();
+}
+
+bool	Replace::isprintableString(std::string lineToCheck) {
+
+	for (int i = 0; i < int(lineToCheck.length()); i++) {
+	
+		if (!isprint(lineToCheck[i]))
+			return false;
+	}
+
+	return true;
+}
+
+bool	Replace::isspaceString(std::string lineToCheck) {
+
+	for (int i = 0; i < int(lineToCheck.length()); i++) {
+	
+		if (!isspace(lineToCheck[i]))
+			return false;
+	}
+	return true;
+}
+
+void	Replace::checkInput() {
+
+	if (Replace::fileName == "" |
+	    Replace::oldText == "" |
+	    Replace::newText == "") {
+		throw std::runtime_error("Input is empty"); }
+	
+	if (!Replace::isprintableString(Replace::fileName) |
+	    !Replace::isprintableString(Replace::oldText)  |
+	    !Replace::isprintableString(Replace::newText)) {
+		throw std::runtime_error("Input is empty");  }
+	
+	if (Replace::isspaceString(Replace::fileName) |
+	    Replace::isspaceString(Replace::oldText) |
+	    Replace::isspaceString(Replace::newText)) {
+		throw std::runtime_error("Input is empty"); }
+}
+
+std::string	Replace::readFile() {
+
+	std::ifstream infile(Replace::fileName.c_str());
+	if (!infile) {
+		std::cerr << "[" << Replace::fileName << "]" << std::endl;
+		throw std::runtime_error("Impossible to open the file"); }
+	
+	std::stringstream buffer;
+	buffer << infile.rdbuf();
+	infile.close();
+	return buffer.str();   
 }
 
 Replace::~Replace() {
