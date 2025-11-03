31617d6d4e694257765ccfe828105419b751ff31
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-11-03 16:43:32 +0100

    feat: added argv of main to test easier the program

--- cpp_module_01/ex05/main.cpp
+++ cpp_module_01/ex05/main.cpp
@@ -6,24 +6,22 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 10:39:13 by yukravch          #+#    #+#             */
-/*   Updated: 2025/11/03 11:48:44 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/11/03 16:41:47 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main(){
+int	main(int ac, char** av) {
 
-	Harl	Harl;
-
-	std::string	levels[6] = {"DEBUG", "random stuff", "INFO", "WARNING", "do you know this?", "ERROR"};
-
-	for (int i = 0; i < 6; i++) {
-
-		std::cout << "Function input is: [" << levels[i] << "]" << std::endl;
-		Harl.complain(levels[i]);
-		if (i != 5)
-			std::cout << std::endl;
+	if (ac != 2) {
+		std::cout << "./harl [DEBUG/INFO/WARNING/ERROR]" << std::endl;
+		return 1;
 	}
+		
+	Harl	Harl;
+	
+	std::cout << "Function input is: [" << av[1] << "]" << std::endl;
+	Harl.complain(av[1]);
 	return 0;
 }
