7d84486849ad00c98f144710ddf4e87aa7969fb7
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-11-24 16:22:13 +0100

    feat: start ex01

---
+++ cpp_module_06/ex01/ScalarConverter/ScalarConverter.cpp
@@ -0,0 +1,334 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yukravch <yukravch@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/21 14:47:27 by yukravch          #+#    #+#             */
+/*   Updated: 2025/11/21 14:47:27 by yukravch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+#define CHAR 3
+#define INT 4
+#define FLOAT 5
+#define DOUBLE 6
+#define PSEUDO 7
+#define ERROR -1
+#define OUT_OF_LIM -2147483649
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other) { *this = other; }
+
+ScalarConverter&	ScalarConverter::operator=(const ScalarConverter& other) {
+
+	if (this != &other) {}
+	return *this;
+}
+
+ScalarConverter::~ScalarConverter() {}
+
+
+
+//methods
+
+void	ScalarConverter::convert(std::string input) {
+
+	std::cout << "Your input is [" << input << "]" << std::endl;
+
+	int	type = ScalarConverter::getType(input);
+
+	switch (type) {
+
+		case CHAR:
+			convertToChar(input);
+			break;
+		case INT:
+			convertToInt(input);
+			break;
+		case PSEUDO:
+			convertToPseudo(input);
+			break;
+		case FLOAT:
+			convertToFloat(input);
+			break;
+		case DOUBLE:
+			convertToDouble(input);
+			break;
+		default:
+			std::cout << "Error: Unknown data type" << std::endl << "Put [int]/[char]/[float]/[double]" << std::endl;
+	}
+
+}
+
+int	ScalarConverter::getType(std::string input) {
+
+	if (ScalarConverter::isChar(input))
+		return CHAR;
+	
+	if (ScalarConverter::isPseudoLiteral(input))
+		return PSEUDO;
+	else if (input[0] == '+' || input[0] == '-' || std::isdigit(input[0])) {
+
+
+		if (ScalarConverter::isFloat(input))
+			return FLOAT;
+		if (ScalarConverter::isDouble(input))
+			return DOUBLE;
+		
+		if ((input[0] == '+' || input[0] == '-') && !std::isdigit(input[1]))
+			return ERROR;
+		if (ScalarConverter::isInt(input))
+			return INT;
+	}
+
+	return ERROR;
+}
+
+
+//CONVERT TO <TYPE>
+//
+//
+
+void	ScalarConverter::convertToPseudo(std::string input) {
+
+	if (input == "-inff" || input == "+inff"
+	  || input == "inff" || input == "nanf") {
+
+		std::cout << "char: impossible" << std::endl
+			 << "int: impossible" << std::endl
+			 << "float: " << input << std::endl
+			 << "double: " << input.substr(0, input.length() - 1) << std::endl;
+	}
+	
+	else if (input == "-inf" || input == "+inf"
+	  || input == "inf" || input == "nan") {
+
+		std::cout << "char: impossible" << std::endl
+			 << "int: impossible" << std::endl
+			 << "float: " << input << "f" << std::endl
+			 << "double: " << input << std::endl;
+	
+	}
+}
+
+void	ScalarConverter::convertToChar(std::string input) {
+
+	char	c = '\0';
+	if (input.length() == 1)
+		c = input[0];
+	else if (input.length() == 3)
+		c = input[1];
+
+	ScalarConverter::printChar(static_cast<int>(c));
+	std::cout << "int: " << static_cast<int>(c) << std::endl
+		  << "float: " << static_cast<float>(c) << ".0f" << std::endl
+	          << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(c) << std::endl;
+}
+
+void	ScalarConverter::convertToInt(std::string input) {
+
+	long	l = 0;
+
+	std::istringstream	myStream(input);
+	if (myStream.good())
+		myStream >> l;
+	if (myStream.fail() || l > std::numeric_limits<int>::max() || l < std::numeric_limits<int>::min()) { std::cout << "char: impossible" << std::endl << "int: impossible" << std::endl; }
+	else {
+	
+		ScalarConverter::printChar(static_cast<int>(l));
+		std::cout << "int: " << input << std::endl;
+	}
+	ScalarConverter::printFloat(input);
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(l) << std::endl;
+}
+
+void	ScalarConverter::convertToFloat(std::string input) {
+
+	double	d = 0;
+	
+	std::istringstream	myStream(input);
+	if (myStream.good())
+		myStream >> d;
+	long l = d;
+	if (myStream.fail() || d > std::numeric_limits<int>::max() || d < std::numeric_limits<int>::min()) { std::cout << "char: impossible" << std::endl << "int: impossible" << std::endl; }
+	else {
+	
+		ScalarConverter::printChar(static_cast<int>(d));
+		std::cout << "int: " << static_cast<int>(d) << std::endl;
+	}
+	ScalarConverter::printFloat(input);
+	if (d - l == 0)
+		std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
+	else
+		std::cout << "double: " << d << std::endl;
+
+}
+
+void	ScalarConverter::convertToDouble(std::string input) {
+
+	double	d = 0;
+
+	std::istringstream	myStream(input);
+	if (myStream.good())
+		myStream >> d;
+	long l = d;
+	if (myStream.fail() || d > std::numeric_limits<int>::max() || d < std::numeric_limits<int>::min()) { std::cout << "char: impossible" << std::endl << "int: impossible" << std::endl << "float: impossible" << std::endl; }
+	else {
+		ScalarConverter::printChar(static_cast<int>(d));
+		std::cout << "int: " << static_cast<int>(d) << std::endl;
+	
+		if (d > std::numeric_limits<float>::max() || d < -std::numeric_limits<float>::max()) { std::cout << "float: impossible" << std::endl; }
+		else {
+			if (d - l == 0)
+				std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(d) << "f" << std::endl;
+			else
+				std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
+		}
+	}
+
+	if (d - l == 0)
+		std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(d) << std::endl;
+	else
+		std::cout << "double: " << input << std::endl;
+
+}
+
+//PRINT <TYPE>
+//
+
+void	ScalarConverter::printChar(int c) {
+
+	if (c < 0 || c > 127)
+		std::cout << "char: impossible" << std::endl;
+	else if (!std::isprint(c) || c == ' ')
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: " <<  "'" << static_cast<char>(c) << "'" << std::endl;
+}
+
+void	ScalarConverter::printInt(std::string input) { (void)input; }
+
+void	ScalarConverter::printFloat(std::string input) {
+
+	double	d = 0;
+	long	l = 0;
+	std::istringstream	myStream(input);
+	if (myStream.good()) {
+		myStream >> d;
+		l = d; }
+	else { std::cout << "float: impossible" << std::endl; }
+	if (d > std::numeric_limits<float>::max() || d < -std::numeric_limits<float>::max()) { std::cout << "float: impossible" << std::endl; }
+	else {
+		if (d - l == 0)	
+			std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(d) << "f";
+		else
+			std::cout << "float: " << static_cast<float>(d) << "f";
+
+		std::cout << std::endl;
+	}
+}
+
+void	ScalarConverter::printDouble(std::string input) { 
+
+	(void)input;
+	
+	
+}
+
+
+//BOOL IS<TYPE>
+//
+bool	ScalarConverter::isChar(std::string input) {
+
+	if (input.length() == 1 && !std::isdigit(input[0]))
+		return true;
+	if (input.length() == 3 && input[0] == 39 && input[2] == 39 && !std::isdigit(input[1]))
+		return true;
+	return false;
+}
+
+bool	ScalarConverter::isInt(std::string input) {
+
+	int	start = 0;
+	
+	if (input[0] == '+' || input[0] == '-')
+		start++;
+	if (stringIsDigit(input, start))
+		return true;
+	return false;
+}
+
+bool	ScalarConverter::stringIsDigit(std::string input, int start) {
+
+	for (size_t i = start; i < input.length(); i++) {
+		if (!std::isdigit(input[i]))
+			return false;
+	}
+	return true;
+}
+
+bool	ScalarConverter::isPseudoLiteral(std::string input) {
+
+	if (input == "-inff" || input == "+inff"
+	  || input == "inff" || input == "nanf") { return true; }
+	
+	if (input == "-inf" || input == "+inf"
+	  || input == "inf" || input == "nan") { return true; }
+	
+	return false;
+}
+
+bool	ScalarConverter::isFloat(std::string input) {
+
+	int	dot = 0;
+	int	len = input.length();
+
+	if (input[len - 1] != 'f')
+		return false;
+
+	int	start = 0;
+
+	if (input[0] == '+' || input[0] == '-')
+		start++;
+	for (int i = start; i < len; i++) {
+	
+		if (!std::isdigit(input[i])) {
+			if (input[i] == '.')
+				dot++;
+			if (input[i] == 'f' && input[i + 1])
+				return false;
+			if (input[i] != '.' && input[i] != 'f')
+				return false;
+		}
+	}
+	if (dot != 1) { return false; }
+
+	return true;
+}
+
+bool	ScalarConverter::isDouble(std::string input) {
+
+	
+	int	dot = 0;
+	int	start = 0;
+
+	if (input[0] == '+' || input[0] == '-')
+		start++;
+	for (size_t i = start; i < input.length(); i++) {
+	
+		if (!std::isdigit(input[i])) {
+
+			if (input[i] == '.') { dot++; }
+			else { return false; }
+		}
+	}
+
+	if (dot != 1) { return false; }
+
+	return true;
+}
---
+++ cpp_module_06/ex01/ScalarConverter/ScalarConverter.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yukravch <yukravch@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/21 14:47:27 by yukravch          #+#    #+#             */
+/*   Updated: 2025/11/21 14:47:27 by yukravch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <sstream>
+#include <limits>
+#include <string>
+#include <iomanip> 
+
+class ScalarConverter {
+
+public:
+	static void	convert(std::string input);
+
+private:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter& other);
+	ScalarConverter&	operator = (const ScalarConverter& other);
+	~ScalarConverter();
+
+	static int	getType(std::string input);
+	static void	convertToChar(std::string input);
+	static void	convertToInt(std::string input);
+	static void	convertToFloat(std::string input);
+	static void	convertToDouble(std::string input);
+	static void	convertToPseudo(std::string input);
+
+	static void	printChar(int c);
+	static void	printInt(std::string input);
+	static void	printFloat(std::string input);
+	static void	printDouble(std::string input);
+
+
+	static bool	isChar(std::string input);
+	static bool	isInt(std::string input);
+	static bool	isFloat(std::string input);
+	static bool	isDouble(std::string input);
+	static bool	isPseudoLiteral(std::string input);
+	static bool	stringIsDigit(std::string input, int start);
+};
+
+#endif
