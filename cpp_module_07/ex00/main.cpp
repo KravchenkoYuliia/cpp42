f27ea06be9695d6f50734c30242250733a8284b2
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-11-29 18:37:31 +0100

    start of a new module

---
+++ cpp_module_07/ex00/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yukravch <yukravch@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/29 18:23:45 by yukravch          #+#    #+#             */
+/*   Updated: 2025/11/29 18:23:45 by yukravch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+int	main( void ) {
+
+
+	int a = 2;
+	int b = 3;
+	
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	/*
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+*/
+	return 0;
+}
+
